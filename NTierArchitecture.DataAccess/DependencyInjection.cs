b9f9869fc4d737e152157d89ea8d86d3c368826b
Muhammet Emin SUSAM <muhammet_1907konya@hotmail.com>
2024-11-20 21:19:19 +0300

    Dependecy Injectionları oluşturduk.

---
+++ NTierArchitecture.DataAccess/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NTierArchitecture.DataAccess.Context;
+using NTierArchitecture.DataAccess.Repositories;
+using NTierArchitecture.Entities.Models;
+using NTierArchitecture.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccess(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            string connectionstring = configuration.GetConnectionString("SqlServer");
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                opt.UseSqlServer(connectionstring);
+            });
+            services.AddIdentityCore<AppUser>(cfr =>
+            {
+                cfr.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IUnitOfWork>(sv=> sv.GetRequiredService<ApplicationDbContext>());
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            return services;
+        }
+    }
+}
