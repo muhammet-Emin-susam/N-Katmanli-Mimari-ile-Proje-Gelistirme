9a57a8860f1285041d8ac5944902c012252e46a5
Muhammet Emin SUSAM <muhammet_1907konya@hotmail.com>
2024-11-20 17:46:17 +0300

    DataAccess Katmanı Oluşturuldu. Enitityleri Configuration Dosyaları Üzerinden Contexte Bağlanıldı.

---
+++ NTierArchitecture.DataAccess/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTierArchitecture.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.DataAccess.Configurations
+{
+    internal sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(x => x.ID);
+            builder.Property(p => p.Name).HasColumnType("varchar(100)");
+        }
+    }
+}
