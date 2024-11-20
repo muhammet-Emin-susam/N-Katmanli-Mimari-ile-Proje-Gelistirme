72ca4bf6716d6cf15df46838fa2939193431fffa
Muhammet Emin SUSAM <muhammet_1907konya@hotmail.com>
2024-11-20 17:25:01 +0300

    Repository Pattern ve UnitOfWork Pattern Oluşturuldu.

---
+++ NTierArchitecture.Entities/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.Entities.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<T> GetByIdAsync(Expression<Func<T,bool>> expression,CancellationToken cancellationToken);
+
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> expression);
+
+    }
+}
