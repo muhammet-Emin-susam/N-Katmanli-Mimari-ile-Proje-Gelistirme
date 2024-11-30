4ffef79dd6b574d088f4674b24b9b5ed2be919c4
Muhammet Emin SUSAM <muhammet_1907konya@hotmail.com>
2024-11-30 19:12:51 +0300

    Exception Handlerlar yazıldı.

--- NTierArchitecture.WebApi/MiddleWare/ExceptionMiddleware.cs
+++ NTierArchitecture.WebApi/MiddleWare/ExceptionMiddleware.cs
@@ -1,29 +1,39 @@
 ﻿
+using Newtonsoft.Json;
+
 namespace NTierArchitecture.WebApi.MiddleWare
 {
     public sealed class ExceptionMiddleware : IMiddleware
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-			try
-			{
-				await next(context);
-			}
-			catch (Exception e)
-			{
-				await HandleExceptionAsync(context, e);
-			}
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionAsync(context, e);
+            }
         }
 
-		private  Task HandleExceptionAsync(HttpContext context, Exception e)
-		{
-			context.Response.ContentType = "application/json";
-			//context.Response.StatusCode = 500;
-			return context.Response.WriteAsync(new
-			{
-				Message = e.Message
-			}.ToString());
+        private Task HandleExceptionAsync(HttpContext context, Exception e)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = 500;
+            return context.Response.WriteAsync(new ErrorResult
+            {
+                Message = e.Message
+            }.ToString());
 
-		}
-	}
+        }
+        public class ErrorResult
+        {
+            public string Message { get; set; }
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+    }
 }
